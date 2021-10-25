22d6e52f106a167a1795a08a5270e2ac430578f8
BasakKurtLab <79793959+BasakKurtLab@users.noreply.github.com>
2021-10-25 23:23:21 +0200

    first commit

---
+++ header.php
@@ -0,0 +1,15 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Home Page</title>
+
+<!-- CSS only -->
+<link href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.3/dist/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-1BmE4kWBq78iYhFldvKuhfTAU6auU8tT94WrHftjDbrCEXSU1oBoqyl2QvZ6jIW3" crossorigin="anonymous">
+
+<link rel="stylesheet" type="text/css" href="style.css">
+</head>
+<body>
+<h1 id="main_title">CRUD APPLICATION IN PHP</h1>
+
+<div class="container">
